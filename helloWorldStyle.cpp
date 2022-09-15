5588bf8edae7fd3b51b87a07d7896a2f1e63cb0b
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-09-15 18:15:23 +0000

    Added Linter and finished program that prints out two lines with style.

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Van Nguyen All rights reserved.
+//
+// Created by: Van Nguyen
+// Date: September 15th, 2022
+// This program prints out a couple lines of text to the console.
+
+// Allows for input and output
+#include <iostream>
+
+// Outputs two lines of text to the console
+int main() {
+    std::cout << "Hello World! With Style!\n";
+    std::cout << "This class is why I wake up in the morning.\n";
+}
